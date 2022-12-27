d4eb3e09f81c562020f8c28f4511d9edada21314
Natanael Copa <ncopa@alpinelinux.org>
2022-12-27 13:41:09 +0100

    Fix linter warning: G102: Binds to all network interfaces (gosec)
    We don't need to listen on all interfaces during testing.
    
    Fixes commit 3c3912189fd7 (handle data package to agent in the same
    goroutine)

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -265,7 +265,7 @@ func TestNodeToControlPlane(t *testing.T) {
 		&AgentTokenAuthenticationOptions{})
 
 	// start a controlplane server
-	lis, err := net.Listen("tcp", ":0")
+	lis, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
 		panic(err)
 	}
